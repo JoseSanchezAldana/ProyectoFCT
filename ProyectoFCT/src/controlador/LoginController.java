c6a93aa4f141d3e25c1b6e5af905b05410126fe5
FX506 <FX506@192.168.1.156>
2024-05-14 20:09:30 +0200

    Login view and login controller

---
+++ ProyectoFCT/src/controlador/LoginController.java
@@ -0,0 +1,47 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import conexion.ConexionSQL;
+import modelo.Modelo;
+import vista.LoginWindow;
+
+public class LoginController implements ActionListener {
+
+	private LoginWindow loginView;
+	private ConexionSQL conexionSQL;
+	private Modelo modelo;
+
+	public LoginController(LoginWindow loginView, ConexionSQL conexionSQL, Modelo modelo) {
+		this.loginView = loginView;
+		this.conexionSQL = conexionSQL;
+		this.modelo = modelo;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == loginView.getLoginButton()) {
+			try {
+				conexionSQL.query("SELECT nombre, password FROM usuarios WHERE nombre = '"
+						+ loginView.getUserTextField().getText() + "';");
+				if (conexionSQL.getRs().next()) { 
+					System.out.println("Usuario introducido: " + loginView.getUserTextField().getText() + " || Usuario bd: " + conexionSQL.getRs().getString(1));
+					System.out.println("Contraseña introducida: " + loginView.getPasswordField().getText() + " || Contraseña bd: " + conexionSQL.getRs().getString(2));
+					if (conexionSQL.getRs().getString(1).equals(loginView.getUserTextField().getText())
+							&& conexionSQL.getRs().getString(2).equals(loginView.getPasswordField().getText())) {
+						System.out.println("Login correcto");
+					} else {
+						System.out.println("Login incorrecto");
+					}
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
+
+}
